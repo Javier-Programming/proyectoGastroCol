8524e9e63bc33e76190e747139c978baceea61d6
andrestowers03-gif <andrestowers03@gmail.com>
2025-11-26 19:58:25 -0500

    rama feature/social actualizada. se le han agregado atributos, constructores,  metodos tanto getter como setter a las clases del campo reaccion

--- product-backend/src/main/java/com/appGastroCol/product_backend/config/SecurityConfig.java
+++ product-backend/src/main/java/com/appGastroCol/product_backend/config/SecurityConfig.java
@@ -40,6 +40,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/api/comentarios/**").permitAll()
+                        .requestMatchers("/api/reacciones/**").permitAll()
                         .anyRequest().authenticated())
                 .formLogin(form -> form.disable())
                 .httpBasic(basic -> basic.disable())
